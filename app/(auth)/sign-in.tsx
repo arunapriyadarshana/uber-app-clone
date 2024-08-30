e0cc5ce7d34c306fde400c22dbcb6be3850fa36c
Aruna Priyadarshana Bandara <arunapbandara45@gmail.com>
2024-08-30 22:28:47 +0530

    show recent rides

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,19 +1,42 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Text, ScrollView, View, Image } from "react-native";
 import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
 import CustomButton from "@/components/CustomButton";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import OAuth from "@/components/OAuth";
+import { useSignIn } from "@clerk/clerk-expo";
 
 const SignUp = () => {
+   const { signIn, setActive, isLoaded } = useSignIn();
+   const router = useRouter();
   const [form, setform] = useState({
     email: "",
     password: "",
   });
-  const onSignUpPress = () => {
-    console.log(form);
-  };
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form.email, form.password]);
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -25,13 +48,14 @@ const SignUp = () => {
           </Text>
         </View>
         <View className="p-5">
-          
           <InputField
             label="Email"
             placeholder="Enter Your Email"
             icon={icons.email}
             value={form.email}
-            onChange={(event) => setform({ ...form, email: event.nativeEvent.text })}
+            onChange={(event) =>
+              setform({ ...form, email: event.nativeEvent.text })
+            }
           />
           <InputField
             label="Password"
@@ -39,11 +63,13 @@ const SignUp = () => {
             icon={icons.lock}
             secureTextEntry={true}
             value={form.password}
-            onChange={(event) => setform({ ...form, password: event.nativeEvent.text })}
+            onChange={(event) =>
+              setform({ ...form, password: event.nativeEvent.text })
+            }
           />
           <CustomButton
             title="Sign Up"
-            onPress={onSignUpPress}
+            onPress={onSignInPress}
             className="w-full mt-5"
           />
           <OAuth />
